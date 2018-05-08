827763078a3305c7b2e03e2b32cf570f3fa95818
cmungall <cjm@berkeleybop.org>
2018-05-07 23:05:32 -0700

    tests

---
+++ tests/test_csv.py
@@ -0,0 +1,10 @@
+from prefixcommons import CsvTransformer
+
+def test_csv():
+    t = CsvTransformer()
+    t.transform("tests/ids.tsv", "tests/ids-tr.tsv")
+    t2 = CsvTransformer(contract=True)
+    t2.transform("tests/ids-tr.tsv", "tests/ids-tr2.tsv")
+
+
+    
