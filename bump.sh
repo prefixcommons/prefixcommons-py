295bc5c28f92b27dd2cea3c89724ebbaed88a98d
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-07-18 16:48:07 -0700

    fully move over to poetry

--- bump.sh
+++
@@ -1,6 +0,0 @@
-TAG=v`python setup.py --version`
-git add prefixcommons/__init__.py
-git commit --message="Upgrade to $TAG"
-git push
-git tag --annotate $TAG --message="Upgrade to $TAG"
-git push --tags
